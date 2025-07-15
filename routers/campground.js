fc66f0abe7adc5a17eb6a012e40a9dbecf548b39
Prakash Raj <prakashrajk274@gmail.com>
2025-07-15 08:41:57 +0530

    Updated layout and seeds; improved navbar and campgrounds view

--- routers/campground.js
+++ routers/campground.js
@@ -7,25 +7,21 @@ const ExpressError = require('../utils/ExpressEroor');
 const { isLoggedIn } = require('../middleware'); 
 const campgrounds=require('../controller/campgrounds')
 
-// GET all campgrounds
+
 router.get("/",campgrounds.index);
 
-// Form to create new campground
+
 router.get("/new",isLoggedIn,campgrounds.new);
 
-// CREATE campground
+
 router.post("/", isLoggedIn,wrapAsync(campgrounds.create));
 
-// SHOW campground
 router.get("/:id", wrapAsync(campgrounds.show));
 
-// EDIT form
 router.get('/:id/edit', isLoggedIn, campgrounds.edit);
 
-// UPDATE campground
 router.put('/:id', isLoggedIn, wrapAsync(campgrounds.update));
 
-// DELETE campground
 router.delete('/:id', isLoggedIn,campgrounds.delete);
 
 module.exports = router;
